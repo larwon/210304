70e966a6a898fcf12118977fd02993019fe938a4
larwon <74400003+larwon@users.noreply.github.com>
2021-03-06 22:57:51 +0900

    Add files via upload

---
+++ mini_project02_02.php
@@ -0,0 +1,16 @@
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr" />
+<?
+echo "아이디 : $id<br>";
+echo "이름 : $name<br>";
+echo "비밀번호 : $password<br>";
+echo "비밀번호 확인 : $password_confirm<br>";
+echo "성별 : $gender<br>";
+echo "휴대번호 : $phone1 - $phone2 - $phone3<br>";
+echo "주소 : $address<br>";
+echo "영화감상 : $movie<br>";
+echo "독서 : $book<br>";
+echo "쇼핑 : $shop<br>";
+echo "운동 : $sport<br>";
+echo "자기소개 : $intro<br>";
+echo "제목(hidden 타입에서 전달) : $title<br>";
+?>
